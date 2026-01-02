5f9e7475267cb2fbe5501b8c6e4bda5a1adf4a33
Adarsh Singh <anexus5919@gmail.com>
2026-01-03 04:09:15 +0530

    feat(layout): update layout with new font, theme provider, and improved structure; add pricing cards and mode toggle component

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from '@/components/global/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/server'
 import Image from 'next/image'
@@ -31,11 +32,12 @@ const Navigation = ({ user }: Props) => {
     </nav>
     <aside className='flex gap-2 items-center'>
         <Link 
-        href={'/agency'} 
+        href={'/agency/sign-in'} 
         className='bg-primary text-white p-2 px-2 rounded-md hover:bg-primary/80'>
         Login
         </Link>
         <UserButton />
+        <ModeToggle />
     </aside>
   </div>
 }
