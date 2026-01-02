2fcdfc2eb13b5062e9b8e18288d0ca48e79ea64e
Adarsh Singh <anexus5919@gmail.com>
2026-01-03 02:40:42 +0530

    feat(auth): implement sign-in and sign-up pages, add site layout and navigation component

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -8,14 +8,15 @@ export default function Home() {
             <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
               <h1 className="text-9xl font-bold text-center md:text-[300px]">Plura</h1>
             </div>
-            <div className="flex justify-cemter items-center relative md:mt-[-70px]">
+            <div className="flex justify-center items-center relative md:mt-[-70px]">
               <Image 
               src={'/assets/preview.png'}
-              alt = "banner image"
+              alt = "banner-image"
               height={1200}
               width={1200}
               className='rounded-tl-2xl rounded-tr-2xl border-2 border-muted'
               />
+              <div className='bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
             </div>
           </section>
       </>
